ea584259fd1ce451da45ccc53c03e075672bbce0
Iann Gonzalez <gonzi482@newschool.edu>
2021-11-16 20:01:01 -0500

    sd

---
+++ week6lesson/js/week6lesson.js
@@ -0,0 +1,28 @@
+/*Puzzle 00: Change a class name into a random string every x seconds
+Your Name:
+
+Functions to be used:
+
+Pseudocode:
+1. Select an element on the page
+2. Write a function that:
+  - Receive an element as an argument
+	- Randomizes a string somehow and store in a variable
+  - Removes the existing classes from the element
+  - Add the random string as a class name to the element
+3. Set the function to be called every x seconds using setInterval 
+
+
+
+JS Code:
+const randomizeClassName = function(element) {
+	// Randomize a string and store in a variable
+	// TODO: make this better - don't use a super long number
+		const randomString = "silly-class-name-" + Math.random()
+
+	// Remove the existing classes from the element
+  
+
+	// Add the random string as a class name to the element
+
+  */
